d52a63cc15cea32a195d3cf69ef95b86eb30e001
marcos <a00824603@itesm.mx>
2020-11-08 21:47:38 -0700

    First pampu commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,46 @@
 import React from "react";
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-  <div className="App">
-    <h1 className="titulo">TecFind.it</h1>
-    <div className="searchBar">
-      <label>Search by object, tag or user</label>
-      <input type="text" name="busquedaObjeto"/><button>Submit</button>
-    </div>
-    
-  </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/sign-in"}></Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/signin"}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/signup"}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
-  
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route path="/signin" component={Login} />
+              <Route path="/signup" component={SignUp} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
